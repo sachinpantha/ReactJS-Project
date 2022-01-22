f9fcf5b7f174bd42528ebad2fbf99f679e541045
sachin <sachinpantha69@gmail.com>
2022-01-22 15:50:40 +0545

    commited

---
+++ src/index.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+import './index.css'
+import books from './files'
+import Typewriter from "typewriter-effect"
+const Booklist=()=>{
+  return(
+    <div>
+    <div id='type'>
+    <App/>
+    </div>
+    <section className='Booklist'>
+      {
+        books.map((book)=>{
+          const {image,title,author}=book;
+          return(
+            <Book book={book}/>
+          );
+        })
+      }
+    </section>
+    </div>
+  );
+}
+function App(){
+  return(
+  <div className='App'>
+   <Typewriter loop={true} onInit={(typewriter)=>{
+      typewriter.typeString("Presenting BamTech Library").pauseFor(2000).deleteAll().typeString('Created By Sachin pantha').start();
+   }}
+   />
+  </div>
+  );
+}
+const Book=(props)=>{ //Props destructuring
+  const {image,title,author,location}=props.book; //Accessing book object via props
+  return(
+    <article>
+     <img src={image} width="195px" height="300px" alt="" />
+     <h3 style={{marginTop:'0.5rem',fontSize:"1.2rem",color:"red"}}>{title}</h3>
+     <h4 style={{fontSize:"0.75rem"}}>{author}</h4>
+     <button type="button" className="btn btn-outline-success" onClick={()=>window.location=location} target="_blank">Read Now!</button>
+    </article>
+  );
+}
+ReactDom.render(<Booklist/>,document.getElementById('root'));
